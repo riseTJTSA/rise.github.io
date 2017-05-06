076e02cb1e6f3b9dd71252d272a76066337c38ba
rithik <rithik@gmail.com>
2017-05-06 09:05:24 -0400

    website push

---
+++ js/renderer.js
@@ -0,0 +1,20 @@
+// This file is required by the index.html file and will
+// be executed in the renderer process for that window.
+// All of the Node.js APIs are available in this process.
+
+// In the main process.
+//const {BrowserWindow} = require('electron')
+
+// Or use `remote` from the renderer process.
+// const {BrowserWindow} = require('electron').remote
+
+// let win = new BrowserWindow({width: 800, height: 600})
+// win.on('closed', () => {
+//   win = null
+// })
+
+// // Load a remote URL
+// win.loadURL('https://github.com')
+
+// // Or load a local HTML file
+// win.loadURL(`file://${__dirname}/../html/loggedIn.html`)
